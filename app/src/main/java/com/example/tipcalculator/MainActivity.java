e18eecc0152ebe38a5a90d90c3bf7030b0ae7ca0
Hari59 <harir2751@gmail.com>
2019-08-23 11:57:51 -0500

    Commit Test 8/23

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void calculatePercent(View v){
+    public void calculatePercent(View v) {
         TextView totalTextView = (TextView) findViewById(R.id.totaltextview);
         EditText billEditText = (EditText) findViewById(R.id.bill);
         TextView tipTextView = (TextView) findViewById(R.id.tip);
@@ -30,52 +30,54 @@ public class MainActivity extends AppCompatActivity {
         CheckBox simpleCheckBox3 = (CheckBox) findViewById(R.id.threestar);
         Boolean checkBox3State = simpleCheckBox3.isChecked();
 
+        int bill = Integer.parseInt(billEditText.getText().toString());
+
         double tip = 0;
 
-        if(checkBox1State){
+        if (checkBox1State) {
             tip = 0.05;
         }
 
-        if(checkBox2State){
+        if (checkBox2State) {
             tip = 0.1;
         }
-        if(checkBox3State){
+        if (checkBox3State) {
             tip = 0.15;
         }
+        double tipAmount = bill*tip;
+        double total = bill + tipAmount;
 
-        double total = tip;
-
-        totalTextView.setText("ok");
+        totalTextView.setText("Total: $" + total);
 
 
     }
 
-    public void oneStarTip(View v){
+    public void oneStarTip(View v) {
         TextView tipTextView = (TextView) findViewById(R.id.tip);
         CheckBox simpleCheckBox = (CheckBox) findViewById(R.id.onestar);
         Boolean checkBoxState = simpleCheckBox.isChecked();
 
-        if(checkBoxState){
+        if (checkBoxState) {
             tipTextView.setText("5%");
         }
     }
 
-    public void twoStarTip(View v){
+    public void twoStarTip(View v) {
         TextView tipTextView = (TextView) findViewById(R.id.tip);
         CheckBox simpleCheckBox = (CheckBox) findViewById(R.id.twostar);
         Boolean checkBoxState = simpleCheckBox.isChecked();
 
-        if(checkBoxState){
+        if (checkBoxState) {
             tipTextView.setText("10%");
         }
     }
 
-    public void threeStarTip(View v){
+    public void threeStarTip(View v) {
         TextView tipTextView = (TextView) findViewById(R.id.tip);
         CheckBox simpleCheckBox = (CheckBox) findViewById(R.id.threestar);
         Boolean checkBoxState = simpleCheckBox.isChecked();
 
-        if(checkBoxState){
+        if (checkBoxState) {
             tipTextView.setText("15%");
         }
     }
